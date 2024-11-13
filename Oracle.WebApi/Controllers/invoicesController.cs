a52f489876547925412f7081bb6b58ba96584bf6
secot <secoto12@gmail.com>
2024-11-13 14:13:01 -0600

    .

---
+++ Oracle.WebApi/Controllers/invoicesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Oracle.DataAccess.Models;
+
+namespace Oracle.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class invoicesController : ControllerBase
+    {
+        private ModelContext _context;
+
+        public invoicesController(ModelContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]// listar facturas
+        public async Task<List<Invoice>> ListarFacturas()
+        {
+
+            return await _context.Invoices.ToListAsync();
+        }
+
+        [HttpPost]//generar factura
+        public async Task<IActionResult> CrearFactura([FromBody] Invoice invoice)
+        {
+            if (!_context.Orders.Any(o => o.Id == invoice.OrderId))
+            {
+                return BadRequest("El ID de la orden no existe.");
+            }
+
+            _context.Invoices.Add(invoice);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(CrearFactura), new { id = invoice.Id }, invoice);
+        }
+
+
+    }
+}
