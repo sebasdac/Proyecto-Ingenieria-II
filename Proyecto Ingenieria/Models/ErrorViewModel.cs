c4f63d9301ffe4c83df785bdff791175cc1e931e
secot <secoto12@gmail.com>
2024-11-13 18:04:45 -0600

    MASTER ARREGLADO FUNCIONAL CON APIS

--- Proyecto Ingenieria/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Proyecto Ingenieria/Program.cs
+++ Proyecto Ingenieria/Program.cs
@@ -4,29 +4,28 @@ using Oracle.DataAccess.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-
-
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=user}/{action=Index}/{id?}");
 
 app.Run();
